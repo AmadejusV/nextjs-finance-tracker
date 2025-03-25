d609205765196c994e81677aea1c81a0a2fb4a5b
Amadejus Valionis <avalionis@reiz.tech>
2025-03-25 15:26:50 +0200

    Fix edit page

--- src/app/edit/[year]/page.tsx
+++ src/app/edit/[year]/page.tsx
@@ -1,7 +1,27 @@
 import { EditYear } from "@/components/financeTracker/EditYear";
 
-const EditYearExpenses = async ({ params }: { params: { year: string } }) => {
-  return <EditYear year={params.year} />;
+// mocked pages since the app is using indexedDb only which runs on client and can't be called here, hence this hacky thing...
+
+export const generateStaticParams = async () => {
+  const MOCK_START_YEAR = 1975;
+  const currentYear = new Date().getFullYear();
+  const allowedYears: { year: string }[] = [];
+
+  for (let i = MOCK_START_YEAR; i <= currentYear; i++) {
+    allowedYears.push({ year: i.toString() });
+  }
+
+  return allowedYears;
+};
+
+const EditYearExpenses = async ({
+  params,
+}: {
+  params: Promise<{ year: string }>;
+}) => {
+  const { year } = await params;
+
+  return <EditYear year={year} />;
 };
 
 export default EditYearExpenses;
