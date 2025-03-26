1f1f002ae58d5e967fc68dacac21687c4092bf95
Amadejus Valionis <avalionis@reiz.tech>
2025-03-26 13:51:32 +0200

    whatever bruh

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,26 @@
 const CACHE_NAME = "my-nextjs-pwa-cache";
 const urlsToCache = [
-  "",
-  "manifest.json",
-  "acounts_icon.png",
-  "addNewExpense",
-  "edit/${yearlyData.year}",
+  "/",
+  "/manifest.json",
+  "/accounts_icon.png",
+  "/addNewExpense",
+  "/edit/[year]",
 ];
+
 self.addEventListener("install", (event) => {
+  console.log("adding install listener");
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(async (cache) => {
+      console.log("adding", { cache });
+      return await cache.addAll(urlsToCache).catch((error) => {
+        console.error("Failed to cache:", error);
+      });
+    })
   );
 });
+
 self.addEventListener("fetch", (event) => {
+  console.log("adding fetch listener");
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
